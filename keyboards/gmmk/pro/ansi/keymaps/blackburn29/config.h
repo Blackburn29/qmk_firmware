3ee495258d35ad1872dd4a6f6ff837a7852bb6ad
Blake LaFleur <blake.k.lafleur@gmail.com>
2021-09-27 16:07:11 -0500

    Remove USB timeout

--- keyboards/gmmk/pro/ansi/keymaps/blackburn29/config.h
+++ keyboards/gmmk/pro/ansi/keymaps/blackburn29/config.h
@@ -17,8 +17,8 @@
 #pragma once
 
 #ifdef RGB_MATRIX_ENABLE
-    #define RGB_DISABLE_TIMEOUT 1200000     // 20 minutes (20 * 60 * 1000ms)
-    #define RGB_DISABLE_WHEN_USB_SUSPENDED
+    //#define RGB_DISABLE_TIMEOUT 1200000     // 20 minutes (20 * 60 * 1000ms)
+    //#define RGB_DISABLE_WHEN_USB_SUSPENDED
     #define RGB_MATRIX_FRAMEBUFFER_EFFECTS // Heatmap, Rain
     #define RGB_MATRIX_KEYPRESSES // REACTIVE, SPLASH modes
 #endif
